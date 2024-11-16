a5270efea337eabb0b47f2ef2252895e586efe6a
Ramadhaneasy <127509500+Ramadhaneasy@users.noreply.github.com>
2024-11-16 08:36:33 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -73,74 +73,116 @@
 //     );
 // }
 
-import React from "react"; 
+// iniasli
+// import React from "react"; 
 
-// Komponen tombol berwarna
-function ButtonBlue() {
+// // Komponen tombol berwarna
+// function ButtonBlue() {
+//     return (
+//         <button
+//             className="h-10 px-6 font-semibold rounded-md bg-blue-500 text-white"
+//             type="submit"
+//         >
+//             Belanja
+//         </button>
+//     );
+// }
+
+// function ButtonYellow() {
+//     return (
+//         <button
+//             className="h-10 px-6 font-semibold rounded-md bg-yellow-500 text-white"
+//             type="submit"
+//         >
+//             Belanja
+//         </button>
+//     );
+// }
+
+// function ButtonBlack() {
+//     return (
+//         <button
+//             className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+//       type="submit"
+//         >
+//             Belanja
+//         </button>
+//     );
+// }
+
+// function ButtonRed() {
+//     return (
+//         <button
+//             className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
+//             type="submit"
+//         >
+//             Belanja
+//         </button>
+//     );
+// }
+
+// function ButtonGreen() {
+//     return (
+//         <button
+//             className="h-10 px-6 font-semibold rounded-md bg-green-500 text-white"
+//             type="submit"
+//         >
+//             Belanja
+//         </button>
+//     );
+// }
+
+// export default function App() {
+//     return (
+//         <div className="text-2xl flex justify-center bg-gray-100 min-h-screen items-center">
+//             <div className="flex gap-4">
+//                 <ButtonBlue />
+//                 <ButtonYellow />
+//                 <ButtonBlack />
+//                 <ButtonRed />
+//                 <ButtonGreen />
+//             </div>
+//         </div>
+//     );
+// }
+
+import React from "react";
+
+// Komponen tombol berwarna menggunakan Props dan Children
+// Functional Component: Sudah ada
+function Button({ bgColor, children }) {
     return (
         <button
-            className="h-10 px-6 font-semibold rounded-md bg-blue-500 text-white"
+            className={`h-10 px-6 font-semibold rounded-md ${bgColor} text-white`}
             type="submit"
         >
-            Belanja
+            {children}
         </button>
     );
 }
 
-function ButtonYellow() {
-    return (
-        <button
-            className="h-10 px-6 font-semibold rounded-md bg-yellow-500 text-white"
-            type="submit"
-        >
-            Belanja
-        </button>
-    );
+// Class Component: Ditambahkan
+class ButtonList extends React.Component {
+    render() {
+        return (
+            <div className="flex gap-4">
+                {/* Menggunakan Props dan Children */}
+                <Button bgColor="bg-blue-500">Belanja</Button> {/* Props dan Children */}
+                <Button bgColor="bg-yellow-500">Belanja</Button> {/* Props dan Children */}
+                <Button bgColor="bg-black">Belanja</Button> {/* Props dan Children */}
+                <Button bgColor="bg-red-500">Belanja</Button> {/* Props dan Children */}
+                <Button bgColor="bg-green-500">Belanja</Button> {/* Props dan Children */}
+            </div>
+        );
+    }
 }
 
-function ButtonBlack() {
-    return (
-        <button
-            className="h-10 px-6 font-semibold rounded-md bg-black text-white hover:bg-blue-500"
-      type="submit"
-        >
-            Belanja
-        </button>
-    );
-}
-
-function ButtonRed() {
-    return (
-        <button
-            className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
-            type="submit"
-        >
-            Belanja
-        </button>
-    );
-}
-
-function ButtonGreen() {
-    return (
-        <button
-            className="h-10 px-6 font-semibold rounded-md bg-green-500 text-white"
-            type="submit"
-        >
-            Belanja
-        </button>
-    );
-}
-
+// Functional Component: Sudah ada
 export default function App() {
     return (
         <div className="text-2xl flex justify-center bg-gray-100 min-h-screen items-center">
-            <div className="flex gap-4">
-                <ButtonBlue />
-                <ButtonYellow />
-                <ButtonBlack />
-                <ButtonRed />
-                <ButtonGreen />
-            </div>
+            {/* Menggunakan Class Component */}
+            <ButtonList /> {/* Class Component */}
         </div>
     );
 }
